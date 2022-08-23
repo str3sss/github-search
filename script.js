4087bccbcd99e063dd24c9933fe0239bd6328408
Dmitry <punkreatine@gmail.com>
2022-08-23 16:09:30 +0300

    fix: removed comments refactored code

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
-const inputSearch = document.querySelector("input");
+const inputSearch = document.getElementById("input");
 const inputContainer = document.querySelector(".dropdown-container");
 const chosens = document.querySelector(".chosens");
 
-chosens.addEventListener("click", function (event) {
+chosens.addEventListener("click", (event) => {
   let target = event.target;
-  if (!target.classList.contains("btn-close")) return;
-
+  if (!target.classList.contains("btn-close")) {
+    return false;
+  }
   target.parentElement.remove();
 });
 
-inputContainer.addEventListener("click", function (event) {
-  let target = event.target; // проверка что мы кликнули именно на dropdown-content 
-  if (!target.classList.contains("dropdown-content")) {
-    return;
-  }
+inputContainer.addEventListener("click", (event) => {
+  let target = event.target;
+  if (!target.classList.contains("dropdown-content")){
+    return false;
+  } 
   addChosen(target);
-  inputSearch.value = ""; // после клика на результат  очищаем поиск и результаты поиска
+  inputSearch.value = "";
   removeSearchResults();
 });
 
-function removeSearchResults() {  // написана 
+function removeSearchResults() {  
   inputContainer.innerHTML = '';
 }
 
-function showPredictions(repositories) {  // написана
+function showPredictions(repositories) {  
   removeSearchResults();
 
   for (let repositoryIndex = 0; repositoryIndex < 5; repositoryIndex++){
@@ -31,22 +32,22 @@ function showPredictions(repositories) {  // написана
     let owner = repositories.items[repositoryIndex].owner.login;
     let stars = repositories.items[repositoryIndex].stargazers_count;
 
-    let dropdownContent = `<div class="dropdown-content" data-owner="${owner}" data-stars="${stars}">${name}</div>`;  // owner и stars хранятся в  датасете; 
-    inputContainer.innerHTML += dropdownContent; // вставляем дивы в выпадающее меню
+    let dropdownContent = `<div class="dropdown-content" data-owner="${owner}" data-stars="${stars}">${name}</div>`; 
+    inputContainer.innerHTML += dropdownContent; 
   }
 }
 
 function addChosen(target) {
-  let name = target.textContent;
-  let owner = target.dataset.owner;
-  let stars = target.dataset.stars;
+  const name = target.textContent;
+  const owner = target.dataset.owner;
+  const stars = target.dataset.stars;
 
   chosens.innerHTML += `<div class="chosen">Name: ${name}<br>Name: ${owner}<br>Stars: ${stars}<button class="btn-close"></button></div>`
 }
 
-async function getSearchResults() { // написана
+async function getSearchResults() { 
   let repositoryName = inputSearch.value
-  if (repositoryName == ''){  // если поле поиска очищается , удалять результаты поиска
+  if (repositoryName === ''){ 
     removeSearchResults();
     return;
   }
@@ -55,17 +56,15 @@ async function getSearchResults() { // написана
   url.searchParams.append('q',repositoryName)
   url.searchParams.append('per_page','5')
   
-  try{
-    let response = await fetch(url)
-    if (response.ok){
-      let repositories = await response.json()
-      showPredictions(repositories);
-    }
-    else return null
+  let response = await fetch(url)
+  if (response.ok){
+    let repositories = await response.json()
+    showPredictions(repositories);
   }
-  catch(e){
-    return null
+  else {
+    console.error('response error',response.status)
   }
+  
 }
 
 function debounce(fn, timeout) {
